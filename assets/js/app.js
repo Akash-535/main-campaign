d2a71a59148eb3d55e2eed7484ad1563c9db49d6
akash-535 <akashbish9e@gmail.com>
2024-10-08 11:00:48 +0530

    section 2 complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,19 +16,33 @@ tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const isActiveTab = document.querySelector('.tab.active');
         if (isActiveTab) {
-                isActiveTab.classList.remove('active', 'bg-twilight', 'text-white', 'py-5', 'px-8', 'font-bold','text-[22px]','max-lg:text-lg','max-md:text-base','max-sm:text-sm');
-            isActiveTab.classList.add('bg-roseWhite', 'text-steel','py-4', 'px-6',);
-        }
-
-        contents.forEach(content => content.classList.add('hidden'));
-        
-        // Add active classes to the clicked tab
-        tab.classList.add('active', 'bg-twilight', 'text-white', 'py-5', 'px-8', 'font-bold','text-[22px]','max-lg:text-lg','max-md:text-base','max-sm:text-sm',);
-        tab.classList.remove('bg-roseWhite', 'text-steel', 'py-4', 'px-6',);
+                isActiveTab.classList.remove('active', 'bg-twilight', 'text-white', 'font-bold',);
+            isActiveTab.classList.add('bg-roseWhite', 'text-steel',); }
+        contents.forEach(content => content.classList.add('hidden'));     
+        tab.classList.add('active', 'bg-twilight', 'text-white', 'font-bold',);
+        tab.classList.remove('bg-roseWhite', 'text-steel',);
         
         const target = tab.getAttribute('data-target');
         const targetContent = document.getElementById(target);
         targetContent && targetContent.classList.remove('hidden');
-        targetContent && targetContent.classList.add('flex');
+    });
+});
+// Tab 2
+const tabTwo = document.querySelectorAll('.tabTwo');
+const contentsTwo = document.querySelectorAll('.tabsInfoTwo');
+
+tabTwo.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const isActiveTab = document.querySelector('.tabTwo.active');
+        if (isActiveTab) {
+                isActiveTab.classList.remove('active', 'bg-twilight', 'text-white', 'font-bold',);
+            isActiveTab.classList.add('bg-roseWhite', 'text-steel',); }
+        contentsTwo.forEach(content => content.classList.add('hidden'));     
+        tab.classList.add('active', 'bg-twilight', 'text-white', 'font-bold',);
+        tab.classList.remove('bg-roseWhite', 'text-steel',);
+        
+        const target = tab.getAttribute('data-target');
+        const targetContent = document.getElementById(target);
+        targetContent && targetContent.classList.remove('hidden');
     });
 });
