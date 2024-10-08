f95c812e90a126402919232ac2080fb960d8e9a3
akash-535 <akashbish9e@gmail.com>
2024-10-08 20:09:26 +0530

    slider added

--- assets/js/app.js
+++ assets/js/app.js
@@ -46,3 +46,46 @@ tabTwo.forEach(tab => {
         targetContent && targetContent.classList.remove('hidden');
     });
 });
+
+// slider
+$(".slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+       slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    infinite: true,
+    centerPadding: '60px',
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        ]
+});
